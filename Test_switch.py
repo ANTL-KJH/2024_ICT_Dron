69d014182108ec97f878806ddae56690e2fb25d1
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-04-18 13:00:40 +0900

    Update Test_switch.py

--- Test_switch.py
+++ Test_switch.py
@@ -1,47 +1,36 @@
 import RPi.GPIO as GPIO
 import time
+import os
 
 # GPIO 핀 번호 설정
 switch1_pin = 11  # 보드 기준 핀 번호
-switch2_pin = 13  # 보드 기준 핀 번호
+switch2_pin = 15  # 보드 기준 핀 번호
 switch3_pin = 36
 switch4_pin = 37
 # GPIO 모드 설정
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(switch1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # 풀 업 설정
-GPIO.setup(switch2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # 풀 업 설정
+GPIO.setup(switch1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 풀 업 설정
+GPIO.setup(switch2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # 풀 업 설정
 GPIO.setup(switch3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(switch4_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
-
-# 스위치 1의 인터럽트 처리 함수
-def switch1_callback(channel):
-    print("스위치 1의 상태가 변경되었습니다.")
-    # 여기에 스위치 1의 상태에 따른 작업을 추가하세요.
-
-# 스위치 2의 인터럽트 처리 함수
-def switch2_callback(channel):
-    print("스위치 2의 상태가 변경되었습니다.")
-    # 여기에 스위치 2의 상태에 따른 작업을 추가하세요.
-def switch3_callback(channel):
-    print("스위치 3의 상태가 변경되었습니다.")
-    # 여기에 스위치 2의 상태에 따른 작업을 추가하세요.
-
-def switch4_callback(channel):
-    print("스위치 4의 상태가 변경되었습니다.")
-    # 여기에 스위치 2의 상태에 따른 작업을 추가하세요.
-
-# 초기 스위치 상태가 OFF일 때만 인터럽트 이벤트 설정
-GPIO.add_event_detect(switch1_pin, GPIO.FALLING, callback=switch1_callback, bouncetime=200)
-GPIO.add_event_detect(switch2_pin, GPIO.FALLING, callback=switch2_callback, bouncetime=200)
-GPIO.add_event_detect(switch3_pin, GPIO.FALLING, callback=switch3_callback, bouncetime=200)
-GPIO.add_event_detect(switch4_pin, GPIO.FALLING, callback=switch4_callback, bouncetime=200)
-
 try:
     while True:
-        # 메인 루프가 계속 실행되도록 유지
-        time.sleep(1)
+        # 현재 스위치 상태 읽기
+        switch1_state = GPIO.input(switch1_pin)
+        switch2_state = GPIO.input(switch2_pin)
+        switch3_state = GPIO.input(switch3_pin)
+        switch4_state = GPIO.input(switch4_pin)
+
+        os.system('clear')
+        # 스위치 상태 출력
+        print("스위치 1 상태:", "ON" if switch1_state == GPIO.LOW else "OFF")
+        print("스위치 2 상태:", "ON" if switch2_state == GPIO.LOW else "OFF")
+        print("스위치 3 상태:", "ON" if switch3_state == GPIO.LOW else "OFF")
+        print("스위치 4 상태:", "ON" if switch4_state == GPIO.LOW else "OFF")
+
+
+        time.sleep(0.05)
 
 except KeyboardInterrupt:
     GPIO.cleanup()  # GPIO 설정 초기화
