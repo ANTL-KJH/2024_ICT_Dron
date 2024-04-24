89d05086bea65fa3e1b64a20e187928f601d8f74
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-04-24 16:24:16 +0900

    Update Test_RPI_CAM3.py

--- Test/Test_RPI_CAM3.py
+++ Test/Test_RPI_CAM3.py
@@ -2,17 +2,22 @@ import cv2
 from picamera2 import Picamera2
 
 picam2 = Picamera2()
-#picam2.preview_configuration.main.size = (1280, 720)
 picam2.preview_configuration.main.size = (640, 480)
-picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.main.format = "BGR888"  # BGR 형식으로 변경
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
+
 while True:
     im = picam2.capture_array()
-    print(im)
-    print(type(im))
+
+    # OpenCV imshow를 사용하여 이미지 표시
     cv2.imshow("Camera", im)
-    #with open("output.jpg", 'rb') as im:
-    #    image_bytes = im.read()
-    #print(image_bytes)
+
+    # 키 입력을 기다리고 'q'가 입력되면 루프를 종료
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# OpenCV 창 닫기 및 Picamera2 종료
+cv2.destroyAllWindows()
+picam2.stop()
