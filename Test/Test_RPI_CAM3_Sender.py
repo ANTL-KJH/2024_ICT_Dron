cee9865cd4694ca5a02b8651062cc33280cfab08
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-04-26 12:56:15 +0900

    Update Test_RPI_CAM3_Sender.py

--- Test/Test_RPI_CAM3_Sender.py
+++ Test/Test_RPI_CAM3_Sender.py
@@ -33,13 +33,12 @@ while True:
     for i in range(num_packets):
         start = i * max_packet_size
         end = min((i + 1) * max_packet_size, size)
-        #client_socket.sendto(data[start:end], (receiver_ip, port))
-        client_socket.sendto(bytes([1]), (receiver_ip, port))
+        client_socket.sendto(data[start:end], (receiver_ip, port))
+        #client_socket.sendto(bytes([1]), (receiver_ip, port))
     # 'q' 키를 누르면 종료
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # 리소스 정리
-cap.release()
 cv2.destroyAllWindows()
 client_socket.close()
