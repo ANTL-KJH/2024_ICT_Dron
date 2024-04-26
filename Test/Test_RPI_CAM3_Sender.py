73059b3a7ce7fd79a9ce2f6e5ebf3d6afdfb9388
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-04-26 12:16:38 +0900

    Update Test_RPI_CAM3_Sender.py

--- Test/Test_RPI_CAM3_Sender.py
+++ Test/Test_RPI_CAM3_Sender.py
@@ -2,11 +2,15 @@ import cv2
 import socket
 import pickle
 import struct
+from picamera2 import Picamera2
 
-# 카메라 초기화
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)  # 영상 가로 크기 설정
-cap.set(4, 480)  # 영상 세로 크기 설정
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (640, 480)
+picam2.preview_configuration.main.format = "BGR888"  # BGR 형식으로 변경
+picam2.preview_configuration.main.format = "RGB888"  # BGR 형식으로 변경
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
 
 # 수신 라즈베리파이의 IP 주소와 포트 번호 설정
 receiver_ip = "192.168.50.126"
@@ -16,10 +20,10 @@ port = 8005
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    ret, frame = cap.read()
-
+    im = picam2.capture_array()
+    cv2.imshow("Camera", im)
     # 영상을 직렬화하여 전송
-    data = pickle.dumps(frame)
+    data = pickle.dumps(im)
     size = len(data)
     max_packet_size = 65000  # UDP 패킷 최대 크기
     num_packets = (size + max_packet_size - 1) // max_packet_size  # 올림 계산
